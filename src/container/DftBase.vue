0c6e9b58fd7321cb563956a15ae779fd94b3860c
schemak@pulipinc.com <schemak@pulipinc.com>
2022-02-28 11:00:42 +0900

    sitemap uddate

--- src/container/DftBase.vue
+++ src/container/DftBase.vue
@@ -1,15 +1,7 @@
+<script setup lang="ts">
+</script>
 <template>
-  <header>
-    <header-area />
-  </header>
   <main class="main">
     <router-view />
   </main>
-  <footer>
-    <footer-area />
-  </footer>
 </template>
-<script setup lang="ts">
-import headerArea from '@/components/header/Header.vue'
-import footerArea from '@/components/footer/Footer.vue'
-</script>
--- src/styles.ts
+++ src/styles.ts
@@ -2,5 +2,4 @@
  * Importing external libraries allow to compile them in our bundle
  * How files are interpreted is defined by ther extension.
  */
-import '@/assets/scss/main.scss';
-import '@/assets/scss/style.scss';
+import '@/assets/css/style.scss';
